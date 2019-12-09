512d7802f92067b36d12ae4271f59eca249814fe
shivam242628 <shivam242628@gmail.com>
2019-12-09 15:20:20 +0530

    implement map based services. closes #6.

---
+++ pet-clinic-data/src/main/java/com/shivam/petclinicapplication/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package com.shivam.petclinicapplication.services.map;
+
+import com.shivam.petclinicapplication.model.Owner;
+import com.shivam.petclinicapplication.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner,Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
