3d0916de0deb9fb1da70395934b7048b1c6e8745
shivam242628 <shivam242628@gmail.com>
2019-12-11 12:17:15 +0530

    add spring configuration to services. closes #22.

--- pet-clinic-data/src/main/java/com/shivam/petclinicapplication/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/shivam/petclinicapplication/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package com.shivam.petclinicapplication.services.map;
 
 import com.shivam.petclinicapplication.model.Pet;
 import com.shivam.petclinicapplication.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
--- pet-clinic-data/src/main/java/com/shivam/petclinicapplication/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/shivam/petclinicapplication/services/map/VetServiceMap.java
@@ -2,9 +2,11 @@ package com.shivam.petclinicapplication.services.map;
 
 import com.shivam.petclinicapplication.model.Vet;
 import com.shivam.petclinicapplication.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService {
 
     @Override
