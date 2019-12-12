0bcdf1e1f5c34c6aa73b537b3669341b40a9793c
shivam242628 <shivam242628@gmail.com>
2019-12-12 14:00:01 +0530

    added visit entity, updated owner with pet relationship. closes #26.

--- pet-clinic-data/src/main/java/com/shivam/petclinicapplication/model/Owner.java
+++ pet-clinic-data/src/main/java/com/shivam/petclinicapplication/model/Owner.java
@@ -1,5 +1,17 @@
 package com.shivam.petclinicapplication.model;
 
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
