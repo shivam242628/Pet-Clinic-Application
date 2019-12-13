2358a9961d5768994284faf9e87bd853df826e6f
shivam242628 <shivam242628@gmail.com>
2019-12-13 18:06:19 +0530

    Enhance Owners with contact info and Pets on startup. closes #33. added specialty service and impl. closes #31.

---
+++ pet-clinic-data/src/main/java/com/shivam/petclinicapplication/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.shivam.petclinicapplication.services;
+
+import com.shivam.petclinicapplication.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long> {
+}
