255f904cac5f9ab03162a89149edf08da6aae84c
shivam242628 <shivam242628@gmail.com>
2019-12-13 17:58:06 +0530

    Enhance Owners with contact info and Pets on startup. closes #33.

--- pet-clinic-data/src/main/java/com/shivam/petclinicapplication/model/Owner.java
+++ pet-clinic-data/src/main/java/com/shivam/petclinicapplication/model/Owner.java
@@ -1,6 +1,7 @@
 package com.shivam.petclinicapplication.model;
 
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Owner extends Person {
@@ -8,7 +9,7 @@ public class Owner extends Person {
     private String address;
     private String city;
     private String telephone;
-    private Set<Pet> pets;
+    private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
         return address;
--- pet-clinic-web/src/main/java/com/shivam/petclinicapplication/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/shivam/petclinicapplication/bootstrap/DataLoader.java
@@ -1,13 +1,17 @@
 package com.shivam.petclinicapplication.bootstrap;
 
 import com.shivam.petclinicapplication.model.Owner;
+import com.shivam.petclinicapplication.model.Pet;
 import com.shivam.petclinicapplication.model.PetType;
 import com.shivam.petclinicapplication.model.Vet;
 import com.shivam.petclinicapplication.services.OwnerService;
 import com.shivam.petclinicapplication.services.PetTypeService;
 import com.shivam.petclinicapplication.services.VetService;
+import org.apache.tomcat.jni.Local;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -37,6 +41,24 @@ public class DataLoader implements CommandLineRunner {
         owner1.setId(1L);
         owner1.setFirstName("Shivam");
         owner1.setLastName("Srivastava");
+        owner1.setAddress("RK Puram");
+        owner1.setCity("New Delhi");
+        owner1.setTelephone("7777777777");
+
+        Pet cotton = new Pet();
+        cotton.setName("Cotton");
+        cotton.setPetType(savedDogPetType);
+        cotton.setOwner(owner1);
+        cotton.setBirthDate(LocalDate.now());
+        owner1.getPets().add(cotton);
+
+        Pet shinzo = new Pet();
+        shinzo.setName("Shinzo");
+        shinzo.setPetType(savedDogPetType);
+        shinzo.setOwner(owner1);
+        shinzo.setBirthDate(LocalDate.now());
+        owner1.getPets().add(shinzo);
+
 
         ownerService.save(owner1);
 
@@ -44,6 +66,23 @@ public class DataLoader implements CommandLineRunner {
         owner2.setId(2L);
         owner2.setFirstName("Arpit");
         owner2.setLastName("Srivastava");
+        owner2.setAddress("Indra Nagar");
+        owner2.setCity("Lucknow");
+        owner2.setTelephone("8888888888");
+
+        Pet myCat = new Pet();
+        myCat.setName("Garfield");
+        myCat.setOwner(owner2);
+        myCat.setPetType(savedCatPetType);
+        myCat.setBirthDate(LocalDate.now());
+        owner2.getPets().add(myCat);
+
+        myCat = new Pet();
+        myCat.setName("Meoww");
+        myCat.setOwner(owner2);
+        myCat.setPetType(savedCatPetType);
+        myCat.setBirthDate(LocalDate.now());
+        owner2.getPets().add(myCat);
 
         ownerService.save(owner2);
 
